cf3561b1404d3d3e470433c82ec123b2c5b3c1bd
Shen <dev@shenlim.com>
2019-04-16 00:45:04 -0500

    Updated forecast loop.

--- Simulation_2.R
+++ Simulation_2.R
@@ -5,11 +5,7 @@ library(ggplot2)
 # Load historical demand data
 # Full, 730-day historical data is available for Calopeia
 # Historical data for other regions only available >= day 640
-demand <- read.csv('2019-04-15 - Demand history.csv', header = TRUE)
-
-demand_cal <- data.frame(day = demand[1:365, 'day'],
-                         year1 = demand[1:365, 'Calopeia'],
-                         year2 = demand[366:730, 'Calopeia'])
+demand <- read.csv('https://raw.githubusercontent.com/shenlim/MSCI3920/master/demand.csv')
 
 demand_oth <- data.frame(demand[640:nrow(demand), names(demand) != 'Calopeia'])
 
@@ -20,7 +16,13 @@ average <- function(weight) {
   while (weight < 0 || weight > 1) {
     stop('Weights must be between 0 and 1 inclusive.')
   }
-  demand_cal$average <<- demand_cal$year1 * (1 - weight) + demand_cal$year2 * weight
+  
+  demand_cal <<- data.frame(demand[, c('day', 'Calopeia')])
+  
+  for (i in seq(nrow(demand_cal) / 2)) {
+    demand_cal[nrow(demand_cal) + 1, ] <<-
+      list(i + 730, demand_cal[i, 2] * (1 - weight) + demand_cal[i + 365, 2] * weight)
+  }
 }
 
 average(0.7)
@@ -28,8 +30,10 @@ average(0.7)
 # Plot demand
 ggplot(data = melt(demand_cal, id.vars = 'day')) +
   geom_line(aes(x = day, y = value)) +
-  facet_wrap(~variable)
+  facet_wrap(~variable) +
+  ylab('demand')
 
 ggplot(data = melt(demand_oth, id.vars = 'day')) +
   geom_line(aes(x = day, y = value)) +
-  facet_wrap(~variable)
+  facet_wrap(~variable) +
+  ylab('demand')
